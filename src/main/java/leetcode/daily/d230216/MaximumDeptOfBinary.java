2fc7703db8ed6c67ec07d73cdae106cae6695fe8
peter-vu <vutuananh.vn.sg@gmail.com>
2023-02-16 14:15:12 +0800

    Add new file

---
+++ src/main/java/leetcode/daily/d230216/MaximumDeptOfBinary.java
@@ -0,0 +1,34 @@
+package leetcode.daily.d230216;
+
+public class MaximumDeptOfBinary {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return 0;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
