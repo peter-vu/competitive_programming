bff41a57011c0da91d48a5db53e45b62efc62d89
peter-vu <vutuananh.vn.sg@gmail.com>
2023-03-01 20:48:09 +0800

    Commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,10 +3,18 @@ import java.util.Optional;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        Long a = Long.valueOf (12345);
+        Long a = Long.valueOf(12345);
         Long b = Long.parseLong("12345");
-        Long c = Long.valueOf (12345);
+        Long c = Long.valueOf(12345);
         System.out.println(a == b);
         System.out.println(a == c);
+
+        Long e = -129L;
+        Long f = -129L;
+        System.out.println(e == f);
+
+        Boolean a1 = Boolean.parseBoolean("true");
+        Boolean b1 = Boolean.TRUE;
+        System.out.println(a1 == b1);
     }
 }
