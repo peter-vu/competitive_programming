e63dc677a897150dc51f1d24904d187693b8dd9b
peter-vu <vutuananh.vn.sg@gmail.com>
2023-04-05 22:24:17 +0800

    Commit

---
+++ src/main/java/leetcode/daily/d230405/MinMaxSum.java
@@ -0,0 +1,19 @@
+package leetcode.daily.d230405;
+
+public class MinMaxSum {
+}
+
+class Solution {
+    public int minimizeArrayValue(int[] nums) {
+        // Initialize answer and the prefix sum.
+        long answer = 0, prefixSum = 0;
+
+        // Iterate over nums, update prefix sum and answer.
+        for (int i = 0; i < nums.length; ++i) {
+            prefixSum += nums[i];
+            answer = Math.max(answer, (prefixSum + i) / (i + 1));
+        }
+
+        return (int) answer;
+    }
+}
