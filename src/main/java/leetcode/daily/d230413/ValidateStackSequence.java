60ab7334957436c1ea6bf7cf086c6ca1f50b19d0
peter-vu <vutuananh.vn.sg@gmail.com>
2023-04-14 21:41:54 +0800

    Commit

---
+++ src/main/java/leetcode/daily/d230413/ValidateStackSequence.java
@@ -0,0 +1,23 @@
+package leetcode.daily.d230413;
+
+import java.util.LinkedList;
+
+public class ValidateStackSequence {
+}
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int N = pushed.length;
+        LinkedList<Integer> stack = new LinkedList<>();
+
+        int j = 0;
+        for (int x: pushed) {
+            stack.addFirst(x);
+            while (!stack.isEmpty() && j < N && stack.getFirst().equals(popped[j])) {
+                stack.removeFirst();
+                j++;
+            }
+        }
+        return j == N;
+    }
+}
