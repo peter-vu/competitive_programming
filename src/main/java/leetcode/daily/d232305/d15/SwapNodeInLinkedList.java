5434b67a84839896456e4f51c8127c68572c09d0
peter-vu <vutuananh.vn.sg@gmail.com>
2023-05-15 21:54:36 +0800

    Commit

---
+++ src/main/java/leetcode/daily/d232305/d15/SwapNodeInLinkedList.java
@@ -0,0 +1,48 @@
+package leetcode.daily.d232305.d15;
+
+public class SwapNodeInLinkedList {
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        int listLength = 0;
+        ListNode frontNode = null;
+        ListNode endNode = null;
+        ListNode currentNode = head;
+        // set the front node and end node in single pass
+        while (currentNode != null) {
+            listLength++;
+            if (endNode != null)
+                endNode = endNode.next;
+            // check if we have reached kth node
+            if (listLength == k) {
+                frontNode = currentNode;
+                endNode = head;
+            }
+            currentNode = currentNode.next;
+        }
+        // swap the values of front node and end node
+        int temp = frontNode.val;
+        frontNode.val = endNode.val;
+        endNode.val = temp;
+        return head;
+    }
+}
